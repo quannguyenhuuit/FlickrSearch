2cac34dc0225dca6e28f9a90c6a1aa69c1aa1224
quanmacos <quanmacos@quanmacoss-Mac.local>
2017-04-29 20:29:41 +0700

    FlickrSearch

---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by quanmacos on 4/29/17.
+//  Copyright © 2017 quanmacos. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
